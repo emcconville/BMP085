2b47f23b322aebf7f55ababc53e8747fe80ddcad
Eric McConville <erichasemail@gmail.com>
2013-11-07 09:05:08 -0600

    Quickly sketched out calculate methods

--- BMP085.py
+++ BMP085.py
@@ -11,6 +11,11 @@ class BMP085(SMBus):
   AC6 = 0x00
   B1  = 0x00
   B2  = 0x00
+  B3  = 0x00
+  B4  = 0x00
+  B5  = 0x00
+  B6  = 0x00
+  B7  = 0x00
   MB  = 0x00
   MC  = 0x00
   MD  = 0x00
@@ -40,6 +45,28 @@ class BMP085(SMBus):
     lsb = self.read_byte(self.port)
     xlsb = self.read_byte(self.port)
     return (msb << 16 | lsb << 8 | xlsb) >> (8-self.oversampling_setting)
+  def calculateTemperature(deviceTemperature):
+    self.B5 = (((deviceTemperature - self.AC6) * self.AC5) >> 0x0F) \
+            + ((self.MC << 0x0B)/(sample1 + self.MD))
+    return (self.B5 + 0x08) >> 0x04
+  def calculatePressure(devicePressure):
+    self.B6 = self.B5 - 0x0FA0
+    temp = (self.B2 * (self.B6 * self.B6) >> 0x0C) >> 0x0B \
+         + (self.AC2 * self.B6) >> 0x0B
+    self.B3 = ((self.AC1 * 4 + temp) << self.oversampling_setting) + 2 >> 0x02
+    x1 = (self.AC3 * self.B6) >> 0x0D
+    x2 = (self.B1 * ((self.B6 * self.B6) >> 0x0C)) >> 0x10
+    x3 = ((x1 + x2) + 2) >> 0x02
+    self.B4 = (self.AC4 * (x3 + 0x8000)) >> 0x0F
+    self.B7 = ((devicePressure - self.B3) * (0xC350 >> self.oversampling_setting))
+    if self.B7 < 0x80000000 :
+      p = (self.B7 << 1) / self.B4
+    else :
+      p = (self.B7 / self.B4) << 1;
+    x1 = ((p >> 8) * (p >> 8) * 3038) >> 0x10
+    x2 = (-7357 * p) >> 0x10;
+    return p + (x1 + x2 + 3791) >> 0x04
+    
   def __calibration__(self):
     self.AC1 = self.readInt(0xAA)
     self.AC2 = self.readInt(0xAC)
